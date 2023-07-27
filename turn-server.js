c9a5da61f50a75af05dbb4f732e882ddf4cc1e22
anuj <anuj@localhost.localdomain>
2023-07-27 14:38:01 +0530

    add host

--- turn-server.js
+++ turn-server.js
@@ -1,9 +1,13 @@
 var Turn = require('node-turn');
-let dotenv = require('dotenv')
+let dotenv = require('dotenv');
+const express = require('express')
+let app = express()
 dotenv.config()
 let PORT = process.env.PORT || 5010
 
 console.log("the current port is",PORT)
+
+app.set('host', process.env.HOST || '0.0.0.0');
 var server = new Turn({
   // set options
   listeningPort:PORT,
