bb5bebb3c64be404207b2b77270e474a33335295
anshtiwari314 <anshtiwari314@gmail.com>
2025-02-14 18:33:02 +0530

    turn-server modified

--- turn-server.js
+++ turn-server.js
@@ -1,20 +1,34 @@
-var Turn = require('node-turn');
+
 let dotenv = require('dotenv');
 const express = require('express')
+const cors  = require('cors')
 let app = express()
 dotenv.config()
-let PORT = process.env.PORT || 5010
+let PORT = process.env.PORT || 3000
 let HOST = process.env.HOST || '127.0.0.1'
+
 console.log("the current port and host",PORT,HOST)
 
-app.set('host', HOST);
-console.log()
-var server = new Turn({
-  // set options
-  listeningPort:PORT,
-  authMech: 'long-term',
-  credentials: {
-    anshtiwari314: "jokanomy-jarvis"
-  }
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get("/", (req, res) => {
+  // console.log('req is coming to server',req)
+ //res.sendFile(path.join(__dirname, "dist", "index.html"));
+ res.json({result:'ok'})
+})
+
+app.get("/getPage", (req, res) => {
+   //console.log('req is coming to server',req)
+ //res.sendFile(path.join(__dirname, "dist", "index2.html"));
+ res.json({result:'ok'})
+})
+//app.set('host', HOST);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
-server.start();
